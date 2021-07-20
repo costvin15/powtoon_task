ce057dc223b28900111b8b682e7aba8a3704680e
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-20 16:18:00 -0300

    feature(app/powtoon): Powtoon app improved
    Powtoon model implemented

--- powtoon/models.py
+++ powtoon/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Powtoon(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.JSONField()
+    owner = models.ForeignKey(User, related_name='powtoons', on_delete=models.CASCADE)
+    shared_with = models.ManyToManyField(User, through='PowtoonShared')
+
+
+class PowtoonShared(models.Model):
+    powtoon = models.ForeignKey(Powtoon, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
