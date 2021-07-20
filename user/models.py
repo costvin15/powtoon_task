78c5b5d7d8b33b63d65af5d929c5168000a647fa
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-20 20:37:27 -0300

    feature(app/user): User app improved
    URLs configuration implemented

--- user/models.py
+++ user/models.py
@@ -5,7 +5,6 @@ from share.models import Group
 class User(models.Model):
     name = models.CharField(max_length=100, blank=True, default='')
     email = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=65, blank=False)
     created = models.DateTimeField(auto_now_add=True)
     groups = models.ManyToManyField(Group, through='UserGroup')
 
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from user import views
 
 router = DefaultRouter()
+router.register(r'user', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
