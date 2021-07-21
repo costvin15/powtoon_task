ff1c3df39fbcebb9664e883856512821047b8b34
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-20 22:49:45 -0300

    feature(app/user): User app improved
    JWT implemented

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token
 from user import views
 
 router = DefaultRouter()
@@ -8,6 +9,6 @@ router.register(r'user', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    url(r'auth/', include('rest_auth.urls')),
+    url(r'auth/login', obtain_jwt_token),
     url(r'auth/register/', include('rest_auth.registration.urls'))
 ]
