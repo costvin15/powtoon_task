5da51327ac4ad4c7f5f375be3cdc9413313380ce
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-21 20:53:22 -0300

    feature(app/user): User app improved
    Groups field added on serialization

--- user/serializers.py
+++ user/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer
 from user.models import User
+from share.serializers import GroupSerializer
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(WritableNestedModelSerializer, serializers.HyperlinkedModelSerializer):
+    groups = GroupSerializer(many=True)
+
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'groups']
