d6544c35ecf12d16064995fe3818767f686ce1f8
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-20 22:33:40 -0300

    feature(app/user): User app improved
    Login fixed

--- user/models.py
+++ user/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from share.models import Group
 
 from user.managers import UserManager
 
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     name = models.CharField(max_length=100, blank=True, default='')
+    username = models.CharField(max_length=130, blank=False, default='')
     email = models.EmailField(max_length=255, unique=True, default='')
     password = models.CharField(max_length=130, blank=False, default='')
     created = models.DateTimeField(auto_now_add=True)
@@ -14,7 +15,7 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [email, password]
-    objects = UserManager
+    objects = UserManager()
 
 
 class UserGroup(models.Model):
