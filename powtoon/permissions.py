21f7b8138f1d7708a40ff869a1ee272dad858909
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-21 22:02:35 -0300

    feature(app/powtoon): Powtoon app improved
    Only powtoon owner can edit or delete

--- powtoon/permissions.py
+++ powtoon/permissions.py
@@ -3,6 +3,6 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.owner.id == request.user.id or obj.shared_with.filter(pk=request.user.id):
+        if obj.owner.id == request.user.id or (request.method in permissions.SAFE_METHODS and obj.shared_with.filter(pk=request.user.id)):
             return True
         return False
