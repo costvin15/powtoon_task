52bf8bad320a7d2ee39241cce21f6d6052211043
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-22 14:06:06 -0300

    feature(app/user): Test cases for User has been implemented

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+
+class UserViewSetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_register(self):
+        self.client.post('/auth/register', {
+            'email': 'user@user.com',
+            'password1': 'password',
+            'password2': 'password'
+        })
+
+    def test_login(self):
+        self.client.post('/auth/login', {
+            'email': 'user@user.com',
+            'password': 'password'
+        })
