355b846ad395e88329624280456ffbdc0babebf9
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-21 23:25:10 -0300

    feature(app/user): User app improved
    Add to group action implemented

--- user/views.py
+++ user/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from user.serializers import UserSerializer
 from powtoon.models import Powtoon
 from powtoon.serializers import PowtoonSerializer
+from share.models import Group
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -17,4 +18,13 @@ class UserViewSet(viewsets.ModelViewSet):
         user = self.get_object()
         powtoons = Powtoon.objects.filter(shared_with__pk=user.pk)
         serializer = PowtoonSerializer(powtoons, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['post'])
+    def add_to_group(self, request, *args, **kwargs):
+        user = self.get_object()
+        group = Group.objects.get(pk=request.data.get('group'))
+        user.groups.add(group)
+        user.save()
+        serializer = UserSerializer(user)
         return Response(serializer.data)
