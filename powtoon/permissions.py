6a32f45a9884321b401bc520a78a559486d879ec
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-21 23:57:05 -0300

    feature(app/powtoon): Powtoon app improved
    Permissions improved to include group permission

--- powtoon/permissions.py
+++ powtoon/permissions.py
@@ -3,6 +3,16 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.owner.id == request.user.id or (request.method in permissions.SAFE_METHODS and obj.shared_with.filter(pk=request.user.id)):
+        if obj.owner.id == request.user.id:
             return True
+        if request.method in permissions.SAFE_METHODS:
+            if obj.shared_with.filter(pk=request.user.id):
+                return True
+            groups = request.user.groups.all()
+            if [group for group in groups if group.permissions.filter(code='perm-view-powtoon-not-shared-or-owner')]:
+                return True
+        else:
+            groups = request.user.groups.all()
+            if [group for group in groups if group.permissions.filter(code='perm-share-powtoon')]:
+                return True
         return False
