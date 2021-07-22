db6f5003342c1e486779e39534a86762e9d231df
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-22 00:26:00 -0300

    task(documentation): Swagger added

--- app/settings.py
+++ app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_auth.registration',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
     'user',
     'share',
     'powtoon',
--- app/urls.py
+++ app/urls.py
@@ -14,9 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Powtoon API",
+      default_version='v1',
+      contact=openapi.Contact(email="costacastrovinicius7@gmail.com"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('', include('user.urls')),
     path('', include('powtoon.urls')),
     path('', include('share.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redoc-old/', schema_view.with_ui('redoc-old', cache_timeout=0), name='schema-redoc-old'),
 ]
