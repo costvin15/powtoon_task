3af038ac848b0d4a076d368c258a3a767c9232a3
Vinicius Castro <costacastrovinicius7@gmail.com>
2021-07-21 21:46:43 -0300

    feature(app/user): User app improved
    Action for listing the shared powtoons with a user implemented

--- user/views.py
+++ user/views.py
@@ -1,9 +1,20 @@
-from rest_framework import viewsets
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from user.serializers import UserSerializer
+from powtoon.models import Powtoon
+from powtoon.serializers import PowtoonSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     User = get_user_model()
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=True)
+    def shared_with_me(self, request, *args, **kwargs):
+        user = self.get_object()
+        powtoons = Powtoon.objects.filter(shared_with__pk=user.pk)
+        serializer = PowtoonSerializer(powtoons, many=True)
+        return Response(serializer.data)
